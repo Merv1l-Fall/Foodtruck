41143d8af68c2d28097838d7bd25a75dd7451210
Vilmer Fall <vilmer.fall@gmail.com>
2024-12-06 14:04:58 +0100

    started working on creating menu items

--- JS/DOM.js
+++ JS/DOM.js
@@ -1,8 +1,33 @@
 import { wontonItems } from "./fetching.js";
 
+const menuContainer = document.querySelector('#menu-container')
 
 
 
-wontonItems.forEach{
+wontonItems.forEach(item => {
+	const menuItem = document.createElement('div');
+	menuItem.classList.add('menu-item');
+	
+	const menuItemInner = document.createElement('div');
+	menuItemInner.classList.add('menu-item-inner');
+	
+	const NameElement = document.createElement('p');
+	NameElement.innerText = item.name;
+	
+	const dottedDivider = document.createElement('div');
+	dottedDivider.classList.add('dotted.divider');
 
-}
+	const priceElement = document.createElement('p');
+ 	priceElement.innerText = item.price;
+
+	 menuItemInner.appendChild(nameElement);
+	 menuItemInner.appendChild(dottedDivider);
+	 menuItemInner.appendChild(priceElement)
+
+	 const ingredientsElement = document.createElement('p');ingredientsElement.innerText = item.ingredients;
+
+	 menuItem.appendChild(menuItemInner);
+	 menuItem.appendChild(ingredientsElement);
+
+	 container.appendChild(menuItem);
+})
