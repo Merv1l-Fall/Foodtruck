0faea388e4d7a06ec8beffeaf3d5144973bcc32e
Vilmer Fall <Vilmer.fall@gmail.com>
2024-12-08 02:31:38 +0100

    making sure prices are handled correctly in submenu

--- JS/DOM.js
+++ JS/DOM.js
@@ -5,6 +5,7 @@ const wonton = 'wonton'
 const drink = 'drink'
 const dip = 'dip'
 
+//getting the menu items and calling the correct function
 async function fetchMenuItems(type) {
 	const Url = `https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/menu?type=${type}`;
 	const items = await getMenuItems(Url);
@@ -32,7 +33,7 @@ function createMenu(items){
 	console.log(items);
 
 		items.forEach(item => {
-			const menuItem = document.createElement('div');
+			const menuItem = document.createElement('button');
 			menuItem.classList.add('menu-item');
 			
 			const menuItemInner = document.createElement('div');
@@ -52,6 +53,7 @@ function createMenu(items){
 			menuItemInner.appendChild(priceElement)
 		
 			const ingredientsElement = document.createElement('p');
+			ingredientsElement.classList.add('ingredients')
 			ingredientsElement.innerText = item.ingredients.join(', ');
 		
 			menuItem.appendChild(menuItemInner);
@@ -61,15 +63,37 @@ function createMenu(items){
 		})
 }
 
+
+//Creating the submenus and showing them
 function createSubMenu(items){
 	items.forEach(item => {
-		const subMenuItem = document.createElement('p');
+		const subMenuItem = document.createElement('button');
 		subMenuItem.classList.add('submenu-item');
 		subMenuItem.innerText = item.name;
 
 		const subMenuSelections = document.querySelector(`.submenu-selections[data-type="${item.type}"]`)
 		subMenuSelections.appendChild(subMenuItem);
 	})
+
+	//making sure prices are updated correctly
+	const dipItem = items.find(item => item.type === "dip")
+	const drinkItem = items.find(item => item.type === "drink")
+
+	if(dipItem) {
+		const subMenuDipPrice = document.querySelector('.dip-price')
+		subMenuDipPrice.innerText = `${dipItem.price} SEK`
+
+	} else{
+		console.log("no dip prices found")
+	}
+
+	if(drinkItem){
+		const subMenuDrinkPrice = document.querySelector('.drink-price')
+		subMenuDrinkPrice.innerText = `${drinkItem.price} SEK`
+	} else{
+		console.log("no drink prices found")
+	}
+
 }
 
 fetchMenuItems(wonton);
