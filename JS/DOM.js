a0192d39f67446f6c45819c1d6263b61acb61fa0
Vilmer Fall <Vilmer.fall@gmail.com>
2024-12-12 20:59:44 +0100

    moved cart updater into cart.js

--- JS/DOM.js
+++ JS/DOM.js
@@ -1,5 +1,5 @@
 import { getMenuItems, placeOrder } from "./fetching.js";
-import { cartManager } from './cart.js';
+import { cartManager, updateCart } from './cart.js';
 
 
 
@@ -98,94 +98,7 @@ function createSubMenu(items){
 }
 
 //updating the cart
-function updateCart(){
-	const cartItems = cartManager.getCartItems();
-	const cartInnerContainer = document.querySelector("#cart-inner-container");
-	cartInnerContainer.innerHTML = '';
 
-	let totalPrice = 0;
-	let totalItems = 0;
-
-	cartItems.forEach(item => {
-		const cartItem = document.createElement('div');
-		cartItem.classList.add('cart-item');
-	
-		const cartItemInner = document.createElement('div');
-		cartItemInner.classList.add('cart-item-inner');
-	
-		const cartItemName = document.createElement('p');
-		cartItemName.innerText = item.name;
-	
-		const cartDivider = document.createElement('div');
-		cartDivider.classList.add('dotted-cart-divider');
-	
-		const cartItemPrice = document.createElement('p');
-		cartItemPrice.innerText = `${item.price} SEK`;
-	
-		cartItemInner.appendChild(cartItemName);
-		cartItemInner.appendChild(cartDivider);
-		cartItemInner.appendChild(cartItemPrice);
-	
-		const cartItemCounter = document.createElement('div');
-		cartItemCounter.classList.add('cart-item-counter');
-	
-		const addButton = document.createElement('button');
-		addButton.classList.add('add-button');
-		addButton.innerText = ' + ';
-	
-		const priceCounterElement = document.createElement('p');
-		priceCounterElement.classList.add('amount');
-		priceCounterElement.innerText = `${item.quantity} stycken`;
-	
-		const removeButton = document.createElement('button');
-		removeButton.classList.add('remove-button');
-		removeButton.innerText = ' - ';
-	
-		addButton.addEventListener('click', () => {
-		  cartManager.addItem(item.name, item.price, item.type);
-		  updateCart();
-		});
-	
-		removeButton.addEventListener('click', () => {
-		  cartManager.removeItem(item.name);
-		  updateCart();
-		});
-	
-		cartItemCounter.appendChild(removeButton);
-		cartItemCounter.appendChild(priceCounterElement);
-		cartItemCounter.appendChild(addButton);
-	
-		cartItem.appendChild(cartItemInner);
-		cartItem.appendChild(cartItemCounter);
-	
-		cartInnerContainer.appendChild(cartItem);
-
-		totalPrice += item.price * item.quantity;
-		totalItems += item.quantity;;
-
-	});
-	
-
-	  const totalPriceElement = document.querySelector ('.total-price')
-	  totalPriceElement.innerText = `${totalPrice} SEK`;
-
-	  const totalItemsElement = document.querySelector('.total-items')
-	  if(totalItems <= 0){
-		totalItemsElement.classList.add('display-none')
-
-		payButton.innerText = 'VARUKORGEN ÄR TOM!';
-		payButton.disabled = true;
-		payButton.classList.remove('active')
-	
-	  } else{
-		totalItemsElement.classList.remove('display-none')
-		totalItemsElement.innerText = totalItems;
-
-		payButton.innerText = 'TAKE MY MONEY!';
-		payButton.disabled = false;
-		payButton.classList.add('active')
-	  }
-}
 
 //makes an order and calls to update eta
 async function handleOrder() {
@@ -270,11 +183,6 @@ payButton.addEventListener('click', () => {
 newOrderButton.addEventListener('click', () => {
 	resetOrder();
 })
-
-
-
-
-
 
 //Switching between different views
 const menuSection = document.querySelector('#menu');
