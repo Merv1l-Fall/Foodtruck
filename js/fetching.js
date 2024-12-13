649e4df61316e9f344ab90e6fde30bee4eb7d57e
Vilmer Fall <Vilmer.fall@gmail.com>
2024-12-13 16:05:02 +0100

    code cleanup and minor adjustments

--- js/fetching.js
+++ js/fetching.js
@@ -1,101 +1,93 @@
 import { cartManager } from "./cart.js";
 
-const APIkey = 'yum-JAaNDtW2DyvIHS96';
-const tenant = 'epjp'
+const APIkey = "yum-JAaNDtW2DyvIHS96";
+const tenant = "epjp";
 let wontonItems = [];
 
-const apiUrl = 'https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/'
+const apiUrl = "https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/";
 
-async function getMenuItems(Url){
-	try{
+async function getMenuItems(Url) {
+	try {
 		const response = await fetch(Url, {
-			method: 'GET',
-			headers:{
-					'x-zocom': APIkey,
-					'Content-Type': 'application/json',
-				},
+			method: "GET",
+			headers: {
+				"x-zocom": APIkey,
+				"Content-Type": "application/json",
+			},
 		});
-		if(!response.ok){
-			throw new Error(`HTTP error! Status: ${response.status}`)
+		if (!response.ok) {
+			throw new Error(`HTTP error! Status: ${response.status}`);
 		}
 		const data = await response.json();
 		wontonItems = data;
 		return data.items;
-	}
-	catch(error) {
-		console.log('Error getting menu', error);
+	} catch (error) {
+		console.log("Error getting menu", error);
 		return [];
 	}
 }
 
-
-
-async function placeOrder(cartManager){
+async function placeOrder(cartManager) {
 	const orderData = {
-        items: cartManager.getCartItems().flatMap(item => 
-            Array(item.quantity).fill(Number(item.id))
-        )
-    };
-	console.log("placing order", orderData)
-	try{
+		items: cartManager
+			.getCartItems()
+			.flatMap((item) => Array(item.quantity).fill(Number(item.id))),
+	};
+	// console.log("placing order", orderData)
+	try {
 		const options = {
-			method: 'POST',
+			method: "POST",
 			headers: {
-				"Content-Type": 'application/json',
+				"Content-Type": "application/json",
 				"x-zocom": APIkey,
 			},
-			body: JSON.stringify(orderData)
-		}
+			body: JSON.stringify(orderData),
+		};
 
-		console.log("Order data before sending:", JSON.stringify(orderData));
+		// console.log("Order data before sending:", JSON.stringify(orderData));
 		const response = await fetch(`${apiUrl}${tenant}/orders`, options);
 
 		if (!response.ok) {
 			const errorResponse = await response.text();
-			console.error('Error response', errorResponse);
+			console.error("Error response", errorResponse);
 			throw new Error(`HTTP error! Status: ${response.status}`);
-		  }
-		
+		}
+
 		const data = await response.json();
-		console.log('Order placed sucsessfully', data);
+		// console.log('Order placed sucsessfully', data);
 		return data;
-	}
-
-	catch(error){
-		console.error('Error placing order', error.message);
+	} catch (error) {
+		console.error("Error placing order", error.message);
 		throw error;
-	}
-};
-
-async function getReceipt(data) {
-	if (!data || !data.order || !data.order.id) {
-        throw new Error("Invalid data provided. Missing order ID.");
-    }
-
-	const orderId = data.order.id;
-	console.log('fetching receipt for orderId:', orderId)
-	try{
-		const options = {
-			method: 'GET',
-			headers: {
-				"Content-Type": 'application/json',
-				"x-zocom": APIkey,
-			}
-		}
-
-		const response = await fetch(`${apiUrl}/receipts/${orderId}`, options)
-
-		if(!response.ok) {
-			throw new Error(`HTTP Error ${response.status}`)
-		}
-		const receiptData = await response.json();
-		return receiptData
-		
-	}
-	catch(error){
-		console.error('Error geting receipt', error.message);
 	}
 }
 
+async function getReceipt(data) {
+	if (!data || !data.order || !data.order.id) {
+		throw new Error("Invalid data provided. Missing order ID.");
+	}
 
-export{ getMenuItems, placeOrder, getReceipt };
+	const orderId = data.order.id;
+	// console.log('fetching receipt for orderId:', orderId)
+	try {
+		const options = {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				"x-zocom": APIkey,
+			},
+		};
+
+		const response = await fetch(`${apiUrl}/receipts/${orderId}`, options);
+
+		if (!response.ok) {
+			throw new Error(`HTTP Error ${response.status}`);
+		}
+		const receiptData = await response.json();
+		return receiptData;
+	} catch (error) {
+		console.error("Error geting receipt", error.message);
+	}
+}
+
+export { getMenuItems, placeOrder, getReceipt };
