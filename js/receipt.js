80c27e6c05d966779d1db173eb4d601488d4acc2
Vilmer Fall <Vilmer.fall@gmail.com>
2024-12-13 15:13:18 +0100

    trying to solve name issues

--- js/receipt.js
+++ js/receipt.js
@@ -1,4 +1,4 @@
-import { getReceipt } from "./js/fetching.js";
+import { getReceipt } from "/fetching.js";
 
 export async function handleReceipt(data) {
 	console.log('handling data for receipt', data)
