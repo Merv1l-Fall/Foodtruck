dd7293e3286149c7a4771accb9d30c7912fc365c
Vilmer Fall <Vilmer.fall@gmail.com>
2024-12-13 14:39:06 +0100

    Fix file name casing conflict

--- js/dom.js
+++ js/dom.js
@@ -1,7 +1,7 @@
-import { getMenuItems, placeOrder } from "/js/fetching.js";
-import { cartManager, updateCart } from '/js/cart.js';
-import { handleReceipt } from "/js/receipt.js";
-import { animateToCart } from "/js/animation.js";
+import { getMenuItems, placeOrder } from "./fetching.js";
+import { cartManager, updateCart } from './cart.js';
+import { handleReceipt } from "./receipt.js";
+import { animateToCart } from "./animation.js";
 
 
 
